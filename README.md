635f9ae9fd1e125c9e337708aaf83e9d20172398
pn-coding <pncoding@naver.com>
2024-10-31 14:59:47 +0900

    api setup

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 # 설치항목
 
 - [x] npm i react-router-dom
+- [x] npm i axios
 - [] npm i styled-components
 - [] npm i styled-reset
 - [] swiper
---
+++ src/api.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjMzMzA4YTJmMGJiMTY0ZTJhZDEwNTE0YWIzMDg2ZiIsIm5iZiI6MTczMDM0NzI3OC4yNzMyMzA2LCJzdWIiOiI2MDJmNmI2YjY0ZjcxNjAwNDA1NjI2MDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.FTmLWixxh-8ANyHCvEE5SUx_6l3XvZjBWg0496SrnUc",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
+
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
+
+export const popular = () =>
+  fetch(url("movie/popular"), options).then((res) => res.json());
